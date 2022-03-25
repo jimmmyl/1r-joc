60204900f898c91d649699c6d993313dce54ee36
jimmmyl <101245796+jimmmyl@users.noreply.github.com>
2022-03-25 12:20:16 +0100

    25/3
    ,

--- godot/cosmoviment.gd
+++ godot/cosmoviment.gd
@@ -4,7 +4,7 @@ var velocitat_base = 200
 var velocitat = Vector2.DOWN * velocitat_base
 var gravetat = Vector2.DOWN * 980
 var salt = Vector2.UP * 500
-var doble_salt = 1
+var doble_salt = 2
 
 
 
@@ -14,11 +14,12 @@ func _physics_process(delta):
 		velocitat += Vector2.RIGHT * velocitat_base
 	if Input.is_action_pressed("mou esquerra"):
 		velocitat += Vector2.LEFT * velocitat_base
-	if Input.is_action_pressed("puja")and doble_salt!= 0:
-		doble_salt -= 1
-		velocitat += salt
 	if is_on_floor():
-		doble_salt = 1
+		doble_salt = 2
+	if Input.is_action_just_pressed("puja") and doble_salt > 0:
+		doble_salt -= 1
+		velocitat = salt
+
 	
 	
 	velocitat += gravetat * delta
@@ -26,7 +27,7 @@ func _physics_process(delta):
 	
 	
 	
-	velocitat = move_and_slide(velocitat)
+	velocitat = move_and_slide(velocitat, Vector2.UP)
 	anima(velocitat)
 	
 	
@@ -46,3 +47,7 @@ func anima(velocitat: Vector2):
 	if abs(velocitat.x) < 0.1:
 		animacio.play("quiet")
 
+
+
+func _on_Area2D_body_entered(body):
+	get_tree().change_scene("res://godot/escena1.tscn")
