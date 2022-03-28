4cf780b3d10e47311b5db6c261273b81b7c59a2f
jimmmyl <101245796+jimmmyl@users.noreply.github.com>
2022-03-28 18:59:15 +0200

    casita 28/3

--- godot/cosmoviment.gd
+++ godot/cosmoviment.gd
@@ -5,7 +5,8 @@ var velocitat = Vector2.DOWN * velocitat_base
 var gravetat = Vector2.DOWN * 980
 var salt = Vector2.UP * 500
 var doble_salt = 2
-var positio = Vector2(10,0)
+var positio = Vector2(40,0)
+var positio2 = Vector2(350,-68)
 
 
 
@@ -62,3 +63,7 @@ func _on_Area2D_body_entered(body):
 
 func _on_Area2D2_body_exited(body):
 	position = positio
+
+
+func _on_Area2D3_body_entered(body):
+	position = positio2
